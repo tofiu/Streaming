31ff1f5b36b0702832a19cfce034f93c267eae11
Tiffany Liu <46113070+tofiu@users.noreply.github.com>
2022-07-21 09:44:49 -0700

    Create episode completion genre.sql

---
+++ General/episode completion genre.sql
@@ -0,0 +1,24 @@
+with stage1 as (
+select
+    reporting_series_nm,
+  v69 ,
+  count(distinct CONCAT(video_season_nbr,video_title)) eps
+FROM
+    ent_summary.fd_house_of_brands a left join `i-dss-ent-data.ent_summary.dj_genre_mapping_table_without_local_stations` b on lower(trim(a.reporting_series_nm))=lower(trim(b.show))
+    
+WHERE
+    day_dt BETWEEN '2022-01-01' and '2022-06-30'
+    AND video_full_episode_ind = TRUE
+    AND v69 IS NOT NULL
+    and genre in ('Reality')
+    and LOWER(subscription_state_desc) IN ("trial","sub","discount offer")
+  GROUP BY
+    1,2)
+select 
+  reporting_series_nm,
+  eps,
+  count(distinct v69) users
+from 
+  stage1 
+group by 
+  1,2
